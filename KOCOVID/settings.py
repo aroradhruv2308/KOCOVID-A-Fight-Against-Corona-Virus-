fba42e9c41a8f52592bb0b3ea46fc2f7f11d5612
dhruvarora <aroradhruv2308@gmail.com>
2021-05-14 14:18:32 +0530

    added postgresql database

--- KOCOVID/settings.py
+++ KOCOVID/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w0kt^l^6_h8q#$9w=u%qz$^wt=8c)*)@7-iglw*h&%oj#rp61y'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['kocovid.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -107,10 +102,23 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-w0kt^l^6_h8q#$9w=u%qz$^wt=8c)*)@7-iglw*h&%oj#rp61y'
 
+# SECURITY WARNING: don't run with debug turned on in production!
+
+ALLOWED_HOSTS = ['localhost','0.0.0.0']
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+a = socket.gethostname()
+if a == "dhruvarora-HP-250-G7-Notebook-PC":
+    DEBUG = TEMPLATE_DEBUG = True
+else:
+    DEBUG = TEMPLATE_DEBUG = False
+
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -139,6 +147,7 @@ STATICFILES_DIRS = [
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 
